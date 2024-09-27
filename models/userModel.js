80ddea174ae8cc38a2373fea2cc9b15753a1a729
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-09-27 23:59:39 +0530

    feat: add user model  schema

---
+++ models/userModel.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema(
+  {
+    fullUserName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    salt: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profileImageUrl: {
+      type: String,
+      default: "/images/dp.png",
+    },
+    role: {
+      type: String,
+      enum: ["USER", "ADMIN"],
+      default: "USER",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
