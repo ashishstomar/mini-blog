6d5005ec326576b3f2b4b4a78ef26b804086edaa
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-10-01 23:59:24 +0530

    chore: connect to MongoDB using Mongoose

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const path = require("path");
+const UserRoute = require("./routes/userRoute");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+  .connect("mongodb://localhost:27017/miniblog")
+  .then((e) => console.log("MongoDB Connected"));
+
+app.use(express.urlencoded({ extended: false })); //handle form data
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
@@ -9,6 +17,8 @@ app.set("views", path.resolve("./views"));
 app.get("/", (req, res) => {
   res.render("index");
 });
+
+app.use("/user", UserRoute);
 
 const PORT = 8080;
 app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
