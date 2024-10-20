049f0a94eb7044138da0fb674441dd0bbc87aa15
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-10-20 23:57:18 +0530

    chore: add middleware for form data parsing, cookie handling, and token authentication

--- app.js
+++ app.js
@@ -2,23 +2,28 @@ const express = require("express");
 const path = require("path");
 const UserRoute = require("./routes/userRoute");
 const mongoose = require("mongoose");
-
+const cookieParser = require("cookie-parser");
+const { checkAuthCookie } = require("./middlewares/authentication");
 const app = express();
 
 mongoose
   .connect("mongodb://localhost:27017/miniblog")
   .then((e) => console.log("MongoDB Connected"));
 
-app.use(express.urlencoded({ extended: false })); //handle form data
-
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+app.use(express.urlencoded({ extended: false })); //handle form data
+app.use(cookieParser());
+app.use(checkAuthCookie("token"));
+
 app.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    user: req.user,
+  });
 });
 
 app.use("/user", UserRoute);
 
-const PORT = 8080;
+const PORT = 8000;
 app.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
