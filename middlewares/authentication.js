b2c9979aca9a2c8a99cfbbcf79ae099687441662
ashishstomar <23213257+ashishstomar@users.noreply.github.com>
2024-10-25 16:41:56 +0530

    fix: call next() in auth middleware

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,19 +1,20 @@
-const { validateToken } = require("../services/auth")
+const { validateToken } = require("../services/auth");
 
 function checkAuthCookie(cookieName) {
-    return (req, res, next){
-        const tokenCookieValue = req.cookies[cookieName]
-        if(!tokenCookieValue){
-           return next()
-        }
-
-        try{
-            const userPayload = validateToken(tokenCookieValue)
-            req.user = userPayload
-        }catch(error) {
-           return next();
-        }
+  return (req, res, next) => {
+    const tokenCookieValue = req.cookies[cookieName];
+    if (!tokenCookieValue) {
+      return next();
     }
+
+    try {
+      const userPayload = validateToken(tokenCookieValue);
+      req.user = userPayload;
+      next();
+    } catch (error) {
+      next();
+    }
+  };
 }
 
-module.exports = {checkAuthCookie}
+module.exports = { checkAuthCookie };
